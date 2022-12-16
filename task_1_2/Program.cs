efc9785db5702847b4f62dba92b9b321dd4d50d6
Asy_student <asb_mogilev@mail.ru>
2022-12-16 11:24:21 +0300

    Tasks 1.2 solution

--- task_1_2/Program.cs
+++ task_1_2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, 
+// на вход принимает два числа 
+// выдаёт, какое число большее, а какое меньшее
+Console.Write("Введите первое число: ");
+double number1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double number2 = Convert.ToDouble(Console.ReadLine());
+
+if (number1 ==number2)
+{
+    Console.WriteLine("Введенные числа равны между собой");
+}
+else if (number1 < number2)
+{
+    Console.WriteLine($"Из чисел {number1} и {number2} максимальное число {number2}");
+}
+else 
+Console.WriteLine($"Из чисел {number1} и {number2} максимальное число {number1}");
+
