4e53a61da8699c3f3cbe440b58ba9bf8a072f661
Asy_student <asb_mogilev@mail.ru>
2022-12-17 18:55:09 +0300

    Added task_1_8

---
+++ task_1_8/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 8: Напишите программу, 
+// на вход принимает число (N) 
+// на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите натуральное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+// I вариант 
+// for (int i = 2; i <= N; i = i + 2) Console.Write(" " + i);
+// II вариант
+int i = 2;
+while (i <= N) 
+{
+Console.Write(" " + i); 
+i = i + 2;
+}
